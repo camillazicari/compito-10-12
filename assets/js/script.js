f210d8965071f25638fab1a099254a2e0022b4ca
camillazicari <camyzicari01@gmail.com>
2024-12-10 16:29:33 +0100

    es 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/* ESERCIZIO 1 */
+
 const yourName = document.getElementById('name');
 const btnSave = document.getElementById('save');
 const btnCancel = document.getElementById('cancel');
@@ -19,3 +21,14 @@ btnCancel.addEventListener('click', (e) => {
     localStorage.clear();
     hello.innerText = '';
 })
+
+/* ESERCIZIO 2 */
+const counter = document.getElementById('counter');
+let timer = 0
+timer = parseInt(sessionStorage.getItem('timer'), 10) || 0;
+
+setInterval(() => {
+    timer ++; 
+    counter.innerText = `${timer}`;
+    sessionStorage.setItem('timer', timer);
+}, 1000)
